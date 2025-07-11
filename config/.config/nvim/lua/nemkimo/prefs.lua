a7c0cda13ac9505a45d06f7b14b93aff157e7e40
Tamas Toth <tamas.toth@htecgroup.com>
2025-07-11 15:10:55 +0200

    Add blink auto completion plugin and some vim options

--- config/.config/nvim/lua/nemkimo/prefs.lua
+++ config/.config/nvim/lua/nemkimo/prefs.lua
@@ -14,22 +14,28 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+vim.opt.cursorline = true
 
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.completeopt = "menuone,noselect"
+
+vim.opt.termguicolors = true
+
+vim.opt.showmode = false
 vim.opt.wrap = false
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
